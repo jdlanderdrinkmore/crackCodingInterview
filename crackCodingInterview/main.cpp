ee14a64eb0dda0537f2aac86be4f056a930da7e1
jdlanderdrinkmore <doingj@gmail.com>
2016-06-25 14:43:36 +0800

    Initial Commit

---
+++ crackCodingInterview/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  crackCodingInterview
+//
+//  Created by jdlander on 16/6/25.
+//  Copyright © 2016年 jdlander. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
